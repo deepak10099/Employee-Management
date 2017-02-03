acdf568537411a01bbc6078668cfc89a85105737
Deepak Ahuja <deepak.ahuja@care.com>
2017-02-03 13:18:02 +0530

    Add Employee View Controller Design Complete

--- Employee Management/AddEmployeeViewController.swift
+++ Employee Management/AddEmployeeViewController.swift
@@ -10,17 +10,19 @@ import UIKit
 
 class AddEmployeeViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
 
+    var detailsLabel = ["Designation","DOB","Address","Gender","Hobbies"]
     @IBOutlet weak var addEmployeeTableView: UITableView!
 
     override func viewDidLoad() {
         addEmployeeTableView.delegate = self
         addEmployeeTableView.dataSource = self
-        addEmployeeTableView.register(AddEmployeeCell.classForCoder(), forCellReuseIdentifier: "firstCell")
-        addEmployeeTableView.register(AddEmployeeCell.classForCoder(), forCellReuseIdentifier: "otherCells")
+//        addEmployeeTableView.register(UINib(nibName: "", bundle: <#T##Bundle?#>), forCellReuseIdentifier: <#T##String#>)
+//        addEmployeeTableView.register(AddEmployeeCell.classForCoder(), forCellReuseIdentifier: "firstCell")
+//        addEmployeeTableView.register(AddEmployeeCell.classForCoder(), forCellReuseIdentifier: "otherCells")
     }
 
     override func viewWillAppear(_ animated: Bool) {
-
+        
     }
 
     override func viewDidAppear(_ animated: Bool) {
@@ -32,29 +34,59 @@ class AddEmployeeViewController: UIViewController,UITableViewDelegate, UITableVi
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return (detailsLabel.count + 1)
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         var cell:AddEmployeeCell!
         if  indexPath.row == 0{
-            cell = addEmployeeTableView.dequeueReusableCell(withIdentifier: "firstCell") as! AddEmployeeCell!
-            cell.profilePicImageView = UIImageView(image: UIImage(named: "profileImage.jpg"))
-            cell.nameLabel.text = "Deepak"
+            cell = addEmployeeTableView.dequeueReusableCell(withIdentifier: "firstCell", for: indexPath) as! AddEmployeeCell
         }
         else{
-            cell = addEmployeeTableView.dequeueReusableCell(withIdentifier: "otherCells") as! AddEmployeeCell!
+            cell = addEmployeeTableView.dequeueReusableCell(withIdentifier: "otherCells", for: indexPath) as! AddEmployeeCell
+            if indexPath.row <= 3 {
+                cell.rightArrowButton.isHidden = true
+            }
+            cell.detailsLabel.text = detailsLabel[indexPath.row -  1]
         }
         return cell
     }
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if  indexPath.row == 4{
+
+        }
+
+        if indexPath.row == 5{
+            
+        }
+    }
+
+    @IBAction func submitButtonTapped(_ sender: Any) {
 
     }
+
 }
 
 class AddEmployeeCell: UITableViewCell {
+    @IBOutlet weak var detailsLabel: UILabel!
+    @IBOutlet weak var detailsTextView: UITextView!
+    @IBOutlet weak var rightArrowButton: UIButton!
+    
     @IBOutlet weak var profilePicImageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var changeProfilePicButton: UIButton!
+
+    @IBAction func rightArrowButtonTapped(_ sender: Any) {
+
+    }
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    
 }
